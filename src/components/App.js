4cbdcc61fae7aaad9e4e176cc48a6967b1c87357
NishantBadlani <nishant.03badlani@gmail.com>
2020-10-21 22:03:02 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,20 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [display, setDisplay] = React.useState(false);
+
+  const handleClick = () => {
+    setDisplay(true);
+  };
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleClick}>
+        Click
+      </button>
+      {display ? <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> : null}
     </div>
   );
 }
-
 
 export default App;
